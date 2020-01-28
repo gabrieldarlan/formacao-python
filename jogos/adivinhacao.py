8661dd3f6d61f7cf7109492d89d1683dc9a80dd4
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-01-28 14:36:50 -0300

    implementacao do loop que controla a quantidade de rodadas no jogo

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -3,19 +3,25 @@ print("Bem vindo no jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(input("Digite o seu numero: "))
+while (rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = int(input("Digite o seu numero: "))
 
-acertou = numero_secreto == chute
-maior = numero_secreto < chute
-menor = numero_secreto > chute
+    acertou = numero_secreto == chute
+    maior = numero_secreto < chute
+    menor = numero_secreto > chute
 
-if (acertou):
-    print("Você acertou!")
-else:
-    if (maior):
-        print("Você errou! O seu chute foi maior que o número secreto.")
-    elif (menor):
-        print("Você errou! O seu chute foi menor que o número secreto.")
+    if (acertou):
+        print("Você acertou!")
+    else:
+        if (maior):
+            print("Você errou! O seu chute foi maior que o número secreto.")
+        elif (menor):
+            print("Você errou! O seu chute foi menor que o número secreto.")
+
+    rodada = rodada + 1
 
 print("Fim do jogo")
