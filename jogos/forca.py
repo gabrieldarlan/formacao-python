e03b902ebde5889ab5ee20120b490aedf72fee21
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-01-29 09:01:14 -0300

    implementacao da verificacao de letra

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,7 +4,23 @@ def jogar():
     print("Bem vindo ao jogo de Forca!")
     print("***************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while (not enforcou and not acertou):
+
+        chute = input("Qual é a letra? ")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+        print("jogando...")
+
     print("Fim do jogo")
+
 
 if (__name__ == "__main__"):
     jogar()
