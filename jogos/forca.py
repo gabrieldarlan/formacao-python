cccd7ae80015246937515e0281b654e0090699cd
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-01-29 14:10:31 -0300

    fim da aula 3

--- jogos/forca.py
+++ jogos/forca.py
@@ -5,9 +5,13 @@ def jogar():
     print("***************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
     enforcou = False
     acertou = False
 
+    print(letras_acertadas)
+    
     while (not enforcou and not acertou):
 
         chute = input("Qual é a letra? ")
@@ -16,10 +20,10 @@ def jogar():
         index = 0
         for letra in palavra_secreta:
             if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
+                letras_acertadas[index] = letra   
             index = index + 1
 
-        print("jogando...")
+        print(letras_acertadas)
 
     print("Fim do jogo")
 
