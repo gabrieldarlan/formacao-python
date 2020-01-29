023ed76d9b01721f21effc84199ea877f647be81
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-01-29 09:44:12 -0300

    implementacao da verificacao da entrada do usuario

--- jogos/forca.py
+++ jogos/forca.py
@@ -11,12 +11,14 @@ def jogar():
     while (not enforcou and not acertou):
 
         chute = input("Qual é a letra? ")
+        chute = chute.strip()
 
         index = 0
         for letra in palavra_secreta:
-            if(chute == letra):
+            if(chute.upper() == letra.upper()):
                 print("Encontrei a letra {} na posição {}".format(letra, index))
             index = index + 1
+
         print("jogando...")
 
     print("Fim do jogo")
