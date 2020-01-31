a7466be1ce0aa84832c5858c30aa7a8931eaad12
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-01-31 13:59:09 -0300

    lista de filmes e programas

--- python_orientado_objetos2/modelo.py
+++ python_orientado_objetos2/modelo.py
@@ -25,6 +25,9 @@ class Filme(Programa):
         super().__init__(nome, ano)
         self.duracao = duracao
 
+    def retorna_cadastro(self):
+        pass
+
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
@@ -34,11 +37,16 @@ class Serie(Programa):
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 vingadores.dar_like()
-print(
-    f'Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}')
+print(f'{vingadores.nome} - {vingadores.duracao} : {vingadores.likes}')
 
 atlanta = Serie('atlanta', 2018, 2)
 atlanta.dar_like()
 atlanta.dar_like()
-print(
-    f'Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}')
+print(f'{atlanta.nome} - {atlanta.temporadas} : {atlanta.likes}')
+
+
+filmes_e_series = [vingadores, atlanta]
+
+for programa in filmes_e_series:
+    detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporadas
+    print(f'{programa.nome} - {detalhes} D {programa.likes}')
