eef8aa7ae4efd032822a2d6d27608121edf44687
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-02-01 01:16:05 -0300

    Aula 5 - Modelo de dados Python completo

--- python_orientado_objetos2/modelo.py
+++ python_orientado_objetos2/modelo.py
@@ -41,15 +41,44 @@ class Serie(Programa):
         return f'{self._nome} - {self.ano} - {self.temporadas} temporadas - {self._likes} Likes'
 
 
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+
+    def __len__(self):
+        return len(self._programas)
+
+
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-vingadores.dar_like()
-
 atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolido', 2016, 2)
+
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
 atlanta.dar_like()
 atlanta.dar_like()
+atlanta.dar_like()
 
 
-filmes_e_series = [vingadores, atlanta]
+filmes_e_series = [vingadores, atlanta,  tmep]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
-for programa in filmes_e_series:
+
+print(f'Tamanho do playlist: {len(playlist_fim_de_semana)}')
+
+for programa in playlist_fim_de_semana:
     print(programa)
